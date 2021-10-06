146c4a39389b0a73cac3f5fd2528239a4827e420
Sicotronicoo <martinezholguinluis@gmail.com>
2021-10-06 19:14:03 +0200

    otra

--- src/main/java/com/example/demo/quiz/controlador.java
+++ src/main/java/com/example/demo/quiz/controlador.java
@@ -1,30 +1,17 @@
 package com.example.demo.quiz;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import java.util.ArrayList;
-import java.util.List;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
 @Controller
 public class controlador {
-	@PostMapping("/persistMessage")
-	public String persistMessage(@RequestParam("msg") String msg, HttpServletRequest request) {
-		@SuppressWarnings("unchecked")
-		List<String> messages = (List<String>) request.getSession().getAttribute("MY_SESSION_MESSAGES");
-		if (messages == null) {
-			messages = new ArrayList<>();
-			request.getSession().setAttribute("MY_SESSION_MESSAGES", messages);
-		}
-		messages.add(msg);
-		request.getSession().setAttribute("MY_SESSION_MESSAGES", messages);
-		return "redirect:/session";
-	}
+	int puntuacion;
 
 	@PostMapping("/destroy")
 	public String destroySession(HttpServletRequest request) {
@@ -38,14 +25,6 @@ public class controlador {
 		return "quizindex";
 	}
 
-	@RequestMapping(value = "/quiz", method = RequestMethod.POST)
-	public String quiz_post(HttpSession httpSession,
-			@RequestParam String anioLanzamiento) {
-
-		
-			httpSession.setAttribute("pregunta1", anioLanzamiento);		
-		return "redirect:/quiz1";
-	}
 
 	@RequestMapping(value = "/quiz1", method = RequestMethod.GET)
 	public String quiz1_get() {
@@ -54,10 +33,9 @@ public class controlador {
 	}
 
 	@RequestMapping(value = "/quiz1", method = RequestMethod.POST)
-	public String quiz1_post(HttpSession httpSession,
-			@RequestParam String droga) {
+	public String quiz1_post(HttpSession httpSession, @RequestParam String anioLanzamiento) {
 
-			httpSession.setAttribute("pregunta2", droga);
+		httpSession.setAttribute("pregunta1", anioLanzamiento);
 		return "redirect:/quiz2";
 	}
 
@@ -68,10 +46,9 @@ public class controlador {
 	}
 
 	@RequestMapping(value = "/quiz2", method = RequestMethod.POST)
-	public String quiz2_post(HttpSession httpSession,
-			@RequestParam String temporada) {
-			
-		httpSession.setAttribute("pregunta3", temporada);
+	public String quiz2_post(HttpSession httpSession, @RequestParam String droga) {
+
+		httpSession.setAttribute("pregunta2", droga);
 		return "redirect:/quiz3";
 	}
 
@@ -82,10 +59,9 @@ public class controlador {
 	}
 
 	@RequestMapping(value = "/quiz3", method = RequestMethod.POST)
-	public String quiz3_post(HttpSession httpSession,
-			@RequestParam String enfermedad) {
+	public String quiz3_post(HttpSession httpSession, @RequestParam String temporada) {
 
-		httpSession.setAttribute("pregunta4", enfermedad);
+		httpSession.setAttribute("pregunta3", temporada);
 		return "redirect:/quiz4";
 	}
 
@@ -96,10 +72,9 @@ public class controlador {
 	}
 
 	@RequestMapping(value = "/quiz4", method = RequestMethod.POST)
-	public String quiz4_post(HttpSession httpSession,
-			@RequestParam String nombreSecundario) {
+	public String quiz4_post(HttpSession httpSession, @RequestParam String enfermedad) {
 
-		httpSession.setAttribute("pregunta5", nombreSecundario);
+		httpSession.setAttribute("pregunta4", enfermedad);
 		return "redirect:/quiz5";
 	}
 
@@ -110,10 +85,9 @@ public class controlador {
 	}
 
 	@RequestMapping(value = "/quiz5", method = RequestMethod.POST)
-	public String quiz5_post(HttpSession httpSession,
-			@RequestParam String edad) {
+	public String quiz5_post(HttpSession httpSession, @RequestParam String nombreSecundario) {
 
-		httpSession.setAttribute("pregunta6", edad);
+		httpSession.setAttribute("pregunta5", nombreSecundario);
 		return "redirect:/quiz6";
 	}
 
@@ -124,9 +98,8 @@ public class controlador {
 	}
 
 	@RequestMapping(value = "/quiz6", method = RequestMethod.POST)
-	public String quiz6_post(HttpSession httpSession,
-			@RequestParam String anio) {
-		
+	public String quiz6_post(HttpSession httpSession, @RequestParam String anio) {
+
 		httpSession.setAttribute("pregunta7", anio);
 		return "redirect:/quiz7";
 	}
@@ -138,17 +111,9 @@ public class controlador {
 	}
 
 	@RequestMapping(value = "/quiz7", method = RequestMethod.POST)
-	public String quiz7_post(Model modelo, HttpSession session, @RequestParam String anioLanzamiento,
-			@RequestParam String droga, @RequestParam String temporada, @RequestParam String enfermedad,
-			@RequestParam String nombreSecundario, @RequestParam String edad, @RequestParam String anio) {
-
-		modelo.addAttribute("anioLanzamiento_form", anioLanzamiento);
-		modelo.addAttribute("droga_form", droga);
-		modelo.addAttribute("temporada_form", temporada);
-		modelo.addAttribute("enfermedad_form", enfermedad);
-		modelo.addAttribute("nombreSecundario_form", nombreSecundario);
-		modelo.addAttribute("edad_form", edad);
-		modelo.addAttribute("anio_form", anio);
+	public String quiz7_post(HttpSession httpSession) {
+		
+		String anioLanzamiento = (String) httpSession.getAttribute("pregunta1");
 		return "redirect:/resultado";
 	}
 }
